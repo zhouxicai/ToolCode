a77c7b60dc26d369dc6f83f76b01da59d4ad49a6
zxc <zxc0168@sina.com>
2019-02-26 15:42:30 +0800

    delete

--- Test2019/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "MessageHandler.h"
-#include "TestHandler.h"
-
-int main() {
-	MessageHandler*	m_msgHandler;
-	m_msgHandler = MsgHandlerFactory::getInstance()->create(1);
-	MsgHandlerFactory::getInstance()->destroy(GZ_1, m_msgHandler);
-}
